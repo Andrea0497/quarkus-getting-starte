d965062167e45d2599d97c8aaed8108102447bbd
Andrea0497 <andrea.capriello497@gmail.com>
2026-02-16 21:44:12 +0100

    Add some fields in ModelBase

--- src/main/java/org/acme/model/ModelBase.java
+++ src/main/java/org/acme/model/ModelBase.java
@@ -1,11 +1,15 @@
 package org.acme.model;
 
+import java.time.LocalDateTime;
+
 import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
 import jakarta.persistence.Column;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
 
 @MappedSuperclass
 public abstract class ModelBase extends PanacheEntityBase{
@@ -13,4 +17,19 @@ public abstract class ModelBase extends PanacheEntityBase{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "ID")
     public Long id;
+    @Column(name = "created_at", updatable = false)
+    public LocalDateTime createdAt;
+    @Column(name = "updated_at")
+    public LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        this.createdAt = LocalDateTime.now();
+        this.updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        this.updatedAt = LocalDateTime.now();
+    }
 }
--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,2 +1,4 @@
-INSERT INTO users (ID, first_name, last_name, email) VALUES (1, 'Andrea', 'Capriello', 'andrea.capriello@example.com');
+INSERT INTO users (ID, created_at, updated_at, first_name, last_name, email) 
+VALUES (1, NOW(), NOW(), 'Andrea', 'Capriello', 'andrea.capriello@example.com');
+
 ALTER TABLE users ALTER COLUMN id RESTART WITH 2;
